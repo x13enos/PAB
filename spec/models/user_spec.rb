6a3ff7adbb6dbf978061a43fb1183bb1bdcb52ac
Andres <x3enos@gmail.com>
2017-01-14 21:11:55 +0300

    Added some user’s tests for checking CI

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,2 +1,14 @@
 describe User do
+  describe ".set_default_role" do
+    let(:admin) { build(:user, :role => :admin) }
+    let(:user) { build(:user) }
+
+    it 'should return existing role' do
+      expect(admin.role).to eq("admin")
+    end
+
+    it 'should return set user role' do
+      expect(user.role).to eq("user")
+    end
+  end
 end
